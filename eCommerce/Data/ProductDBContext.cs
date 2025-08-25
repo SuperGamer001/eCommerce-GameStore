8ac01e4b92bbbed3f6e2c12c8772bcfa08ee1c72
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-08-25 14:53:36 -0700

    Add Entity Framework Core support and ProductDBContext
    Updated eCommerce.csproj to include Entity Framework Core
    packages: Microsoft.EntityFrameworkCore.SqlServer and
    Microsoft.EntityFrameworkCore.Tools (v9.0.8).
    
    Added ProductDBContext class for database operations,
    including a DbSet<Product> for managing Product entities.

---
+++ eCommerce/Data/ProductDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.Data;
+
+public class ProductDBContext : DbContext
+{
+    public ProductDBContext(DbContextOptions<ProductDBContext> options) : base(options)
+    {
+    }
+    // Entities to be tracked by DbContext
+    public DbSet<Product> Products { get; set; }
+    }
