df81715f77a64598244fbacd32d7efbf4c5d3502
Alex Fischer <183024988+SuperGamer001@users.noreply.github.com>
2025-08-27 12:55:55 -0500

    Add ProductController and Create product view (#16)
    Introduces a new `ProductController` for managing product actions, including displaying a list of products and creating new products. The controller utilizes `ProductDBContext` for database interactions and includes model validation.
    
    Also adds a `Create.cshtml` Razor view with a form for product input, complete with validation messages and scripts to enhance user experience.

---
+++ eCommerce/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using eCommerce.Data;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductDBContext _context;
+
+        public ProductController(ProductDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the product to the database
+                _context.Products.Add(product);     // Add the new product to the DbSet
+                await _context.SaveChangesAsync(); // Save changes to the database
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+            return View(product);
+        }
+    }
+}
